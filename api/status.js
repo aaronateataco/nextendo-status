3d999eba554e2b001fe775953ea4cf38fd180971
aaronateataco <aaron@aaronworld.uk>
2026-08-26 17:17:27 +0100

    cookie

--- api/status.js
+++ api/status.js
@@ -9,7 +9,9 @@ export default async function handler(req, res) {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0 Safari/537.36",
                 "Accept": "application/json, text/plain, */*",
                 "Accept-Language": "en-US,en;q=0.9",
-                "Referer": "https://nextendo.network/",
+                "Referer": "https://nextendo.network/status",
+                "Connection": "keep-alive",
+                "Cookie": process.env.NEXTENDO_COOKIE
             }
         });
 
